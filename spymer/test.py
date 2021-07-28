1337edbc3a19755ae94ac9d8ca41af6844d041a5
headmasterritual <70149463+headmasterritual@users.noreply.github.com>
2021-07-28 11:46:21 +0300

    Update test.py

--- spymer/test.py
+++ spymer/test.py
@@ -1,3 +1,12 @@
+from locust import HttpLocust
+from locust import TaskSet
+from locust import task
+
+# For HTML reporting
+from locust.web import app
+from src import report
+app.add_url_rule('/htmlreport', 'htmlreport', report.download_report)
+
 class Test_1(TaskSet):
     @task(1)
     def users(self):
